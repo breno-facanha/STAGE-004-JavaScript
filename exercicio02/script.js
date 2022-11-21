a90ea38ce640b495ddcb017216742d9e5ecabc84
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2022-11-21 16:10:54 -0300

    Criado a lista dos estudantes

---
+++ exercicio02/script.js
@@ -0,0 +1,30 @@
+const students = [
+    {
+        name: "Breno" , 
+        notaPrimeiraProva: 6,
+        notaSegundaProva: 9
+    },
+    {
+        name: "Bruno", 
+        notaPrimeiraProva: 7,
+        notaSegundaProva: 6
+    },
+    {
+        name: "Bernado", 
+        notaPrimeiraProva: 3,
+        notaSegundaProva: 9
+    },
+    {
+        name: "Benicio", 
+        notaPrimeiraProva: 5,
+        notaSegundaProva: 9
+    },
+    {
+        name: "Façanha", 
+        notaPrimeiraProva: 7,
+        notaSegundaProva: 8
+    },
+]
+
+
+

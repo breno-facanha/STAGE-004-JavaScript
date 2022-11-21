a9ade22e14ca4422a4d843fc3d6dd9eb9171b706
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2022-11-21 16:01:37 -0300

    Exercicio 01 finalizado
    Feito a
    - soma
    - subtração
    - multiplicação
    - divisão
    - par ou impar
    - números iguais ou não

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,3 +1,42 @@
 const numberOne = Number(prompt('Digite o primeiro numero:'))
 const numberTwo = Number(prompt('Digite o segundo numero:'))
 
+
+const soma = numberOne + numberTwo;
+const subtração = numberOne - numberTwo;
+const multiplicação = numberOne * numberTwo;
+const divisão = numberOne / numberTwo;
+
+let ImparOuPar = soma % 2 == 0 ? 'Par' : 'Impar'
+let NumberEquals = numberOne == numberTwo ? 'iguas' : 'diferentes'
+
+alert(
+    `
+    A Soma dos dois numeros é: ${soma} 
+    `
+)
+alert(
+    `
+    A Subtração dos dois numeros é: ${subtração} 
+    `
+)
+alert(
+    `
+    A Multiplicação dos dois numeros é: ${multiplicação} 
+    `
+)
+alert(
+    `
+    A Divisão dos dois numeros é: ${divisão} 
+    `
+)
+alert(
+    `
+    A Soma dos dois numeros são: ${ImparOuPar} 
+    `
+)
+alert(
+    `
+    Os numéros inseridos são ${NumberEquals} 
+    `
+)
